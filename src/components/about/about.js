10846f8e109db750c0c796228d729bd15866dc43
SMZRDev <69090919+ZafarRizvi@users.noreply.github.com>
2024-06-16 08:27:43 +0500

    v0.1

---
+++ src/components/about/about.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./about.css";
+
+const about = () => {
+  return (
+    <div className="about_main">
+      <div className="about_lower_main">
+        <div className="about_imgs">Images</div>
+        <div className="about_text">
+          <h1>About Me</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default about;
